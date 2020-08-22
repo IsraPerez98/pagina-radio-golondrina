e6a5d8149c4bef7d8af538a2c3d89b61fbfb8ff1
IsraPerez98 <isra1998isra@gmail.com>
2020-08-22 19:48:03 -0400

    barra navegacion con css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import './App.css';
+import './css/fuentes.scss';
 
 import { BrowserRouter, Route } from 'react-router-dom';
 
--- src/componentes/barra-navegacion.js
+++ src/componentes/barra-navegacion.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react';
 import { NavLink } from "react-router-dom";
+import Collapsible from 'react-collapsible';
 
 import favicon from "../img/favicon.png";
+
+import '../css/barra-navegacion.scss';
 
 class BarraNavegacion extends Component {
     render() {
         return(
-            <div className="barra-superior">
-                <div className="fondo-barra-superior"></div>
-                <div className="contenido-barra-superior">
+            <div className="barra-navegacion">
+                <div className="contenido-barra-navegacion">
                     <div className="div-logo">
                         <img
                             className="logo-img"
                             src={favicon}
                             alt="Logo"
                         />
-                        <h1>
-                            <NavLink className="logo-link" to="/">
-                                Radio Golondrina Paine
+                        <NavLink className="logo-link" to="/">
+                            <h1>Radio Golondrina Paine</h1>
+                        </NavLink>    
+                    </div>
+                    <Collapsible 
+                        trigger= {
+                            <div className="boton-navegacion">
+                                <span className="fa fa-bars"></span>
+                            </div>
+                        }
+                    >
+                        <div className="div-navegacion">
+                            <NavLink className="nav-link" exact to="/">
+                                Inicio
                             </NavLink>
-                        </h1>
-                    </div>
-                    <div className="div-navegacion">
-                        <NavLink className="nav-link" exact to="/">
-                            Inicio
-                        </NavLink>
-                    </div>
+                        </div>
+                    </Collapsible>
                 </div>
             </div>
         )
