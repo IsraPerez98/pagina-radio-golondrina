87487f5aa2418cbb44aeafee8c38f743c879fb81
IsraPerez98 <isra1998isra@gmail.com>
2020-08-24 06:10:49 -0400

    cambios al reproductor

--- src/componentes/reproductor.js
+++ src/componentes/reproductor.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import '../css/componentes/reproductor.scss';
+import "../css/componentes/reproductor.scss";
 
 class Reproductor extends Component {
     constructor(props) {
@@ -10,7 +10,7 @@ class Reproductor extends Component {
         this.botonPlay = React.createRef();
         this.botonMute = React.createRef();
         this.barraVolumen = React.createRef();
-        
+
         this.clickBotonPlay = this.clickBotonPlay.bind(this);
         this.clickBotonMute = this.clickBotonMute.bind(this);
         this.cambioBarraVolumen = this.cambioBarraVolumen.bind(this);
@@ -19,7 +19,8 @@ class Reproductor extends Component {
     clickBotonPlay(evento) {
         if (this.audioRef.current.paused) {
             this.botonPlay.current.className = "boton-play stop";
-            this.audioRef.current.src = "https://streaming.radiogolondrina.cl/golondrina128";
+            this.audioRef.current.src =
+                "https://streaming.radiogolondrina.cl/golondrina128";
             this.audioRef.current.play();
         } else {
             this.botonPlay.current.className = "boton-play play";
@@ -29,7 +30,7 @@ class Reproductor extends Component {
     }
 
     clickBotonMute(evento) {
-        if(!this.audioRef.current.muted) {
+        if (!this.audioRef.current.muted) {
             this.audioRef.current.muted = true;
             this.botonMute.current.className = "boton-mute mute";
         } else {
@@ -39,10 +40,10 @@ class Reproductor extends Component {
     }
 
     cambioBarraVolumen(evento) {
-        const volumen = this.barraVolumen.current.value / 100
+        const volumen = this.barraVolumen.current.value / 100;
         this.audioRef.current.volume = volumen;
 
-        if(volumen === 0) {
+        if (volumen === 0) {
             this.audioRef.current.muted = true;
             this.botonMute.current.className = "boton-mute mute";
         } else {
@@ -52,48 +53,51 @@ class Reproductor extends Component {
     }
 
     render() {
-        return(
-            <div className="reproductor">
-                <div className="reproductor-interior">
-                    <div className="reproductor-div-izquierda">
-                        <label className="suena-ahora">Suena Ahora en Golondrina FM</label>
-                        <label className="nombre-programa">SAMPLE TEXT</label>
+        return (
+            <div>
+                <div className="reproductor-espacio"></div>
+                <div className="reproductor">
+                    <div className="reproductor-interior">
+                        <div className="reproductor-div-izquierda">
+                            <label className="suena-ahora">
+                                Suena Ahora en Golondrina FM
+                            </label>
+                            <label className="nombre-programa">
+                                SAMPLE TEXT
+                            </label>
+                        </div>
+                        <div className="reproductor-div-centro">
+                            <button
+                                className="boton-play play"
+                                ref={this.botonPlay}
+                                onClick={this.clickBotonPlay}
+                            ></button>
+                        </div>
+                        <div className="reproductor-div-derecha">
+                            <button
+                                className="boton-mute sonando"
+                                ref={this.botonMute}
+                                onClick={this.clickBotonMute}
+                            ></button>
+                            <input
+                                className="barra-volumen"
+                                type="range"
+                                min="0"
+                                max="100"
+                                step="1"
+                                defaultValue="100"
+                                ref={this.barraVolumen}
+                                onChange={this.cambioBarraVolumen}
+                            ></input>
+                        </div>
                     </div>
-                    <div className="reproductor-div-centro">  
-                        <button 
-                            className="boton-play play"
-                            ref={this.botonPlay}
-                            onClick={this.clickBotonPlay}
-                        >
-                        </button>  
-                    </div>
-                    <div className="reproductor-div-derecha">   
-                        <button 
-                            className="boton-mute sonando"
-                            ref={this.botonMute}
-                            onClick={this.clickBotonMute}
-                        >
-                        </button>
-                        <input 
-                            className="barra-volumen" 
-                            type="range" 
-                            min="0" 
-                            max="100" 
-                            step="1" 
-                            defaultValue="100" 
-                            
-                            ref={this.barraVolumen}
-                            onChange={this.cambioBarraVolumen}
-                        >
-                        </input>
-                    </div>
+                    <audio ref={this.audioRef} className="audio-player">
+                        <source type="audio/mpeg" />
+                        Tu navegador no es compatible con esta caracteristica.
+                    </audio>
                 </div>
-                <audio ref={this.audioRef} className="audio-player">
-                    <source type="audio/mpeg" />
-                    Tu navegador no es compatible con esta caracteristica.
-		        </audio>
             </div>
-        )
+        );
     }
 }
 
