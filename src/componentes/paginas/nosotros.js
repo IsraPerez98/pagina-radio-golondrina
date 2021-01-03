5206a546e48f97247579b86e5701c508017be8e6
IsraPerez98 <isra1998isra@gmail.com>
2021-01-03 05:16:40 -0300

    mapa google

--- src/componentes/paginas/nosotros.js
+++ src/componentes/paginas/nosotros.js
@@ -6,6 +6,9 @@ class Nosotros extends Component {
     render() {
         return (
             <div className="pagina-nosotros">
+                <div className="ubicacion">
+                    <iframe className="iframe-mapa" title="mapa" frameBorder="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d629.2239987529155!2d-70.74701532895544!3d-33.81354969270181!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9663230e848f3c4f%3A0x2606d318a11df7cf!2s1ra%20Compa%C3%B1ia%20de%20Bomberos%20Paine!5e0!3m2!1ses-419!2scl!4v1609659129493!5m2!1ses-419!2scl" ></iframe>
+                </div>
             </div>
         );
     }
